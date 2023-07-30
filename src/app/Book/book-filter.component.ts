0c98cc6d981927fe768a3a8722953961e9e22155
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-07-30 23:53:35 +0530

    buggy code

--- src/app/Book/book-filter.component.ts
+++ src/app/Book/book-filter.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
@@ -13,30 +13,31 @@ import { BookService } from './book.service';
   selector: 'book-store-book-filter',
   standalone: true,
   imports: [
-    CommonModule,
+    NgIf,
+    NgFor,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    ReactiveFormsModule,
     MatIconModule,
     MatSelectModule,
   ],
   template: `
     <div class="filter-container">
-      <div class=".search-filter">
-        <mat-form-field>
+      <div class="search-filter">
+        <mat-form-field appearance="outline">
           <mat-label>Search by title/author</mat-label>
           <input matInput [formControl]="searchTerm" />
           <mat-icon matPrefix>search</mat-icon>
         </mat-form-field>
       </div>
       <div class="language-filter">
-        <!-- <mat-form-field>
+        <mat-form-field appearance="outline">
           <mat-label>Language</mat-label>
-          <mat-select>
+          <mat-select [formControl]="language">
             <mat-option value="1">Option 1</mat-option>
             <mat-option value="2">Option 2</mat-option>
           </mat-select>
-        </mat-form-field> -->
+        </mat-form-field>
       </div>
     </div>
   `,
@@ -45,10 +46,10 @@ import { BookService } from './book.service';
       .filter-container {
         margin: 10px 0px;
         display: flex;
+        gap: 8px;
       }
       .search-filter {
         width: 350px;
-        border: 1px solid;
       }
       mat-form-field {
         width: 100%;
@@ -59,6 +60,7 @@ import { BookService } from './book.service';
 })
 export class BookFilterComponent {
   searchTerm = new FormControl('');
+  language = new FormControl('');
   private readonly bookService = inject(BookService);
 
   constructor() {
