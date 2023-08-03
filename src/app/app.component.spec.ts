17825b88670f1245ffe9bc08c8292b88c688e266
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-08-03 16:24:34 +0530

    app.component unit test closes #3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,26 +1,27 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent, RouterTestingModule],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    component = fixture.componentInstance;
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('h1')?.textContent).toContain(
-  //     'Welcome book-store'
-  //   );
-  // });
+  it('should create an app', () => {
+    expect(component).toBeTruthy();
+  });
 
-  // it(`should have as title 'book-store'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('book-store');
-  // });
+  it('should render the header, content-holder, and footer', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('book-store-header')).toBeTruthy();
+    expect(compiled.querySelector('.content-holder')).toBeTruthy();
+    expect(compiled.querySelector('book-store-footer')).toBeTruthy();
+  });
 });
