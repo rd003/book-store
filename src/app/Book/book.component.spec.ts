a46b5d79228c32f4539b46fea98c6862dd760e6c
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-08-05 11:37:45 +0530

    something

--- src/app/Book/book.component.spec.ts
+++ src/app/Book/book.component.spec.ts
@@ -5,37 +5,11 @@ import { provideRouter } from '@angular/router';
 import { Book } from '@book-store/shared-models';
 import { Observable, of } from 'rxjs';
 import { appRoutes } from '../app.routes';
+import { mockBooks } from '../utils/mock-books.util';
 import { BookFilterComponent } from './book-filter.component';
 import { BookListComponent } from './book-list.component';
 import { BookComponent } from './book.component';
 import { BookService } from './book.service';
-
-const mockBooks: Book[] = [
-  {
-    Id: '8ef9f86b-cdc4-49ab-88de-f641e8d0ab73',
-    Author: 'Chinua Achebe',
-    Country: 'Nigeria',
-    ImageLink: 'assets/images/things-fall-apart.jpg',
-    Language: 'English',
-    Link: 'https://en.wikipedia.org/wiki/Things_Fall_Apart\n',
-    Pages: 209,
-    Title: 'Things Fall Apart',
-    Year: 1958,
-    Price: 243,
-  },
-  {
-    Id: 'ab38cb7b-f4de-4c3f-9463-dcba1bd62f36',
-    Author: 'Hans Christian Andersen',
-    Country: 'Denmark',
-    ImageLink: 'assets/images/fairy-tales.jpg',
-    Language: 'Danish',
-    Link: 'https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n',
-    Pages: 784,
-    Title: 'Fairy tales',
-    Year: 1836,
-    Price: 101,
-  },
-];
 
 //Mock BookService
 class MockBookService {
